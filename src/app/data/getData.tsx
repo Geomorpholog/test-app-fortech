baa053efafd9f98bc6f5ec213f07a8d6210e8002
Geomorpholog <Geomorpholog@rambler.ru>
2024-07-22 02:53:27 +0300

    feat: TS types and project structure improvement

--- src/app/data/getData.tsx
+++ src/app/data/getData.tsx
@@ -5,7 +5,7 @@ export type Data<T> = {
   data:T
 };
 
-export const getData = async (): Promise<Data<User> | undefined> => {
+export const getData = async (): Promise<Data<User[]> | undefined> => {
   const res = await fetch('https://dummyjson.com/users')
   if (!res.ok) {
     throw new Error('Failed to fetch data')
